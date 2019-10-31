d5c51416ee6b7afb5e622b8db1a5f137bf2af475
Wes <wes@wesreed.co>
2019-10-30 20:38:32 -0400

    adds html

--- app/public/wp-content/themes/location/index.php
+++ app/public/wp-content/themes/location/index.php
@@ -1,14 +1,27 @@
-<?php 
-    echo "<h1>Send me your location.</h1>";
+<html>
+    <head>
+        <?php wp_head(); ?>
+    </head>
+</html>
+
+<?php
 
 if(have_posts()) {
     while(have_posts()){
-        the_post();
-        the_title();
-        the_content();
-        the_date(); 
+        the_post(); ?>
+        <section>
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_content(); ?></p>
+            <i><?php the_date(); ?></i>
+        </section>
+<?php
     }    
 }
 else {
     echo '404 Page Not Found!';
 }
+?>
+
+<footer>
+    <?php wp_footer(); ?>
+</footer>
